6ec0f3529739d08162f29239c9f8bdad623cb0e8
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-10-21 12:42:29 +0200

    Release 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures tentacool'
 long_description 'Installs/Configures tentacool'
-version '0.1.0'
+version '0.1.2'
 
 depends 'apt'
